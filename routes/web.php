46c0f9ed1f315029c5bfeba36cee2783f27d7aab
Raghad Suliman <raghadrhh02@gmail.com>
2024-07-23 19:02:42 +0300

    route change

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TableController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -23,16 +25,11 @@ Route::middleware(['auth-check'])->group(function () {
     Route::post('/upload-file' , [DepartmentStorageController::class , 'store'])->name('upload-file');
     Route::get('/show-file' , [DepartmentStorageController::class , 'showfile'])->name('show-file');
     Route::get('/category/{id}', [CategoryController::class, 'show'])->name('category.show');
+    Route::get('/dashboard' , [DashboardController::class , 'index'])->name('dashboard');
 
-    Route::get('/dashboard', function () {
-        return view('dashboard.layouts.home');
-    });
     
     
-    Route::get('/dashboard', function () {
-        return view('dashboard.layouts.home');
-    })->middleware(['auth', 'verified'])->name('dashboard');
-    
+ 
     Route::get('/table',[TableController::class,'table'])->name('table');
 
     Route::get('/category/{id}', [CategoryController::class, 'show'])->name('category.show');
