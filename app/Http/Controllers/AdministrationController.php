ae8f4b89bce4734d7e215c323223629f3925ec72
Najla-fz1 <159172185+Najla-fz1@users.noreply.github.com>
2024-07-24 15:00:55 +0300

    show files with some conditions

---
+++ app/Http/Controllers/AdministrationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\DepartmentStorage;
+use App\Models\Category;
+
+
+class AdministrationController extends Controller
+{
+    public function administrationfiles()
+{
+    $storageItems = DepartmentStorage::with(['department', 'categorys', 'user'])->get();
+
+    $Category=Category::with('departmentStorages');
+
+    return view('dashboard.layouts.administrationfiles', [
+        'storageItems' => $storageItems,
+        'Category'=>$Category,
+    ]);
+}
+
+}
