bdfe6a91018ad88543db0786cd4bc68c54ab477d
shatha <sha7cs@gmail.com>
2024-07-28 19:49:54 +0300

    fixed icons in all files page

--- resources/views/dashboard/layouts/allcategory.blade.php
+++ resources/views/dashboard/layouts/allcategory.blade.php
@@ -31,38 +31,46 @@
                         $cardClass = 'border-info';
                         $cardHeader = 'Document';
                         $cardBodyClass = 'text-info';
+                        $icon = 'fa fa-file';
                         break;
                     case 2:
                         $cardClass = 'border-warning';
                         $cardHeader = 'Power Point';
                         $cardBodyClass = 'text-warning';
+                        $icon = 'fa fa-file-powerpoint-o';
                         break;
                     case 3:
                         $cardClass = 'border-success';
                         $cardHeader = 'Image';
                         $cardBodyClass = 'text-success';
+                        $icon = 'fa fa-file-image-o';
                         break;
                     case 4:
                         $cardClass = 'border-primary';
                         $cardHeader = 'Video';
                         $cardBodyClass = 'text-primary';
+                        $icon = 'fa fa-file-movie-o';
                         break;
                     case 5:
                         $cardClass = 'border-danger';
                         $cardHeader = 'PDF';
                         $cardBodyClass = 'text-danger';
+                        $icon = 'fa fa-file-pdf-o';
                         break;
                     default:
                         $cardClass = 'border-info';
                         $cardHeader = 'Document';
                         $cardBodyClass = '';
+                        $icon = 'fa fa-file-pdf-o';
                         break;
                 }
             @endphp
 
             <div class="card {{ $cardClass }} mb-2 " style="max-width: 18rem; ">
             <a href="{{ Storage::url($item->file) }}" target="_blank" class="text-decoration-none text-reset">
-                <div class="card-header">{{ $cardHeader }}</div>
+                <div class="card-header">
+                    <i class="{{ $icon }}" style="font-size:28px"></i>
+                </div>
                 <div class="card-body {{ $cardBodyClass }}">
                     <h5 class="card-title">{{ $item->title }}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
