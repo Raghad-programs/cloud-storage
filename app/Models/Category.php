28f2a3bd9bf254e7b3b0518d1a43c903a1e60a98
shatha <sha7cs@gmail.com>
2024-07-30 04:50:48 +0300

    added the option to add a new category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,10 @@ class Category extends Model
 {
     use HasFactory;
 
+    protected $fillable =[
+        'name',
+        'department_id'
+    ];
     public function department()
     {
         return $this->belongsTo(Department::class);
