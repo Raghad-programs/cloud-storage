778ef6a762ae66660ecb5dd5b4446082613d3a25
Najla-fz1 <159172185+Najla-fz1@users.noreply.github.com>
2024-08-04 17:09:10 +0300

    welcome email

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use App\Mail\WelcomeEmail;
+use Illuminate\Support\Facades\Mail;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -24,10 +26,19 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
+        // Authenticate the user
         $request->authenticate();
 
+        // Get the authenticated user
+        $user = Auth::user();
+
+        // Send the welcome email
+        Mail::to($user->email)->send(new WelcomeEmail($user->name));
+
+        // Regenerate the session
         $request->session()->regenerate();
 
+        // Redirect the user to the intended destination
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
--- config/mail.php
+++ config/mail.php
@@ -37,17 +37,30 @@ return [
 
     'mailers' => [
 
-        'smtp' => [
-            'transport' => 'smtp',
-            'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
-        ],
+        // 'smtp' => [
+        //     'transport' => 'smtp',
+        //     'url' => env('MAIL_URL'),
+        //     'host' => env('MAIL_HOST', '127.0.0.1'),
+        //     'port' => env('MAIL_PORT', 2525),
+        //     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+        //     'username' => env('MAIL_USERNAME'),
+        //     'password' => env('MAIL_PASSWORD'),
+        //     'timeout' => null,
+        //     'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+        // ],
+
+        'mailers' => [
+    'smtp' => [
+        'transport' => 'smtp',
+        'host' => 'smtp.mailtrap.io',
+        'port' => 2525,
+        'encryption' => 'tls',
+        'username' => 'your_mailtrap_username',
+        'password' => 'your_mailtrap_password',
+        'timeout' => null,
+        'auth_mode' => null,
+    ],
+],
 
         'ses' => [
             'transport' => 'ses',
