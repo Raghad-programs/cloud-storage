4c275afcf065e991792cc76b2ad948065be32b18
Najla-fz1 <159172185+Najla-fz1@users.noreply.github.com>
2024-08-07 08:54:44 +0300

    file creation with editing

--- app/Models/FileType.php
+++ app/Models/FileType.php
@@ -4,14 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FileType extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
     protected $fillable = [
         'type',
         'extensions',
     ];
+
+    
 
     public function departmentStorages()
     {
