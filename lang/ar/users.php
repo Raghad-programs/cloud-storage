99940a706e2d3a08f38d900ded963b878f627d97
shatha <sha7cs@gmail.com>
2024-08-14 14:14:23 +0300

    translated the validation and all ar files

---
+++ lang/ar/users.php
@@ -0,0 +1,17 @@
+<?php
+
+    return [
+        'columns' => [
+            'first_name' => 'الاسم الأول',
+            'last_name' => 'الاسم الأخير',
+            'email' => 'البريد الإلكتروني',
+            'linkedin_url' => 'رابط لينكدين',
+            'email_verified_at' => 'وقت تحقق البريد الإلكتروني',
+            'password' => 'كلمة المرور',
+            'phone_number'=> 'رقم الهاتف',
+            'storage_size'=> 'سعة التخزين',
+            'created_at'=> 'انشئ في',
+            'updated_at'=> 'تم التعديل في ',
+            'deleted_at' => 'تم الحذف في',
+        ]
+    ];
