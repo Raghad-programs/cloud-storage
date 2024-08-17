a6760950544e53c5053a84801d4ec709fba7e8bf
Najla-fz1 <159172185+Najla-fz1@users.noreply.github.com>
2024-08-17 04:10:42 +0300

    my archive translating edit 2

--- lang/ar/showfile.php
+++ lang/ar/showfile.php
@@ -11,4 +11,13 @@ return [
     'Created_At'=>'تاريخ الإنشاء',
     'actions'=>'تحرير',
     'Download_all_files'=>'تنزيل كل الملفات',
+    'Click_to_view_file'=>'انقر لعرض الملف',
+    'Confirm_Deletion'=>'تأكيد الحذف',
+    'Deletion_massage'=>'قم بتحديد "حذف" أدناه إذا كنت متأكدًا من أنك تريد حذف هذا الملف.',
+    'Cancel'=>'الغاء',
+    'Delete'=>'حذف',
+    'Update_File'=>'تعديل الملف',
+    'Update'=>'تعديل',
+    'File_Title'=>'عنوان الملف',
+    'File_Description'=>'وصف الملف',
 ];
